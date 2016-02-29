f5b8efa346e4d915345afb27ccc6a7393d15a88f
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-29 12:02:30 -0800

    added post request to js

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,8 +1,17 @@
 $(document ).ready(function() {
-    $('button').click(function (){
-      console.log(params);
-      // $.post('http://localhost:9393/api/v1/token', {
-      //   email: params['email']
-      // })
+    $('.btn').click(function (){
+      $.post('http://localhost:9393/api/v1/token', {
+        email: params['email'],
+        password: params['password']
+      })
+      .done(function (data) {
+        console.log(data);
+        // document.cookie = ""data['token'] + ;
+        document.location.href = '/index.html';
+      })
+      .fail(function (data){
+        console.log(data);
+        console.log('failed');
+      });
     });
 });
