8cc2a0b814139fb34ff54c030310600b87d26a02
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-29 11:47:22 -0800

    skeletoning some basic pages

---
+++ javascripts/login.js
@@ -0,0 +1,8 @@
+$(document ).ready(function() {
+    $('button').click(function (){
+      console.log(params);
+      // $.post('http://localhost:9393/api/v1/token', {
+      //   email: params['email']
+      // })
+    });
+});
