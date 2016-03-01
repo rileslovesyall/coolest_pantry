70e4b7e95a959f6aa8938528b4faf7d6008b7222
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-01 10:58:10 -0800

    changed pantry display to table:

--- public/pantry.js
+++ public/pantry.js
@@ -7,18 +7,29 @@ $(document).ready(function () {
     headers: {'Authorization': token},
     url: "http://localhost:9393/api/v1/users/" + uid + "/personal_pantry",
     success: function(data) {
-        console.log(data);
+        var length = data['pantry_items'].length;
+        var pantryHtml = "";
+        pantryHtml += "<table id='pantry-table'><th>Item</th><th>Portion Size</th>" +
+        "<th>Stock</th><th>Add More</th><th>Consume</th>";
+        for (i = 0; i < length; i++) {
+          var item  = data['pantry_items'][i];
+            pantryHtml +="<tr>" +
+            "<td class='item_name' id="+item['id']+">" + item["name"] + "</td>" +
+            "<td class='portion_size' id="+item['id']+">" + "FILL ME" + "</td>" +
+            "<td class='stock' id="+item['id']+">" + item["quantity"] + "</td>" +
+            "<td class='add' id="+item['id']+">" + "ADD" + "</td>" +
+            "<td class='consume' id="+item['id']+">" + "CONSUME" + "</td>" +
+            "</tr>";
+        }
+        $('#pantry').html(pantryHtml);
     }
   })
   .done(function (data) {
-    var length = data['pantry_items'].length;
-    var pantryHtml = "";
-    for (i = 0; i < length; i++) {
-      var item  = data['pantry_items'][i];
-        pantryHtml += "<div class='pantryitem' id=" + item['id'] + "> Name: " +
-          item['name'] + "<br> Description: " + item['description'] + "</div>";
-    }
-    $('#pantry').html(pantryHtml);
+    console.log('done');
+  })
+  .fail(function (data) {
+    console.log(data);
+    console.log("Error, this failed.");
   });
 
 });
