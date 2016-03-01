2f50685a5fabfd09a2c2ea5f9bb85da55120244f
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-01 13:18:27 -0800

    added application js to handle things that happen on all pages"

---
+++ public/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('head').append(
+      "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>"+
+      "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' integrity='sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS' crossorigin='anonymous'></script>"+
+      "<link href='https://fonts.googleapis.com/css?family=Special+Elite|Nixie+One' rel='stylesheet' type='text/css'>"+
+      "<link rel='stylesheet' href='../public/style/application.css'>"
+    );
+  console.log("application js works");
+});
