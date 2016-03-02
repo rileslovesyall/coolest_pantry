ee711be1a5016417c72c0858637cb4ce1ce9462d
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-02 06:53:03 -0800

    add pantryitem form being created and serialized properly:

--- public/pantryitem.js
+++ public/pantryitem.js
@@ -1,20 +1,33 @@
 var addItemForm = function () {
   var formHtml = "";
   formHtml +=
-    "<form>" +
+    "<form class='addForm'>" +
       "<fieldset class='form-group'>" +
         "<label for='name'>Name: </label>" +
-        "<input class='form-control' for='name' id='name' type='text'>" +
+        "<input class='form-control' for='name' id='name' type='text' name='name' required>" +
         "<label for='description'>Description </label>" +
-        "<input class='form-control for='description' id='description'>" +
+        "<input class='form-control for='description' id='description' name='description'>" +
         "<label for='portion-size'>Portion Size: </label>" +
-        "<input class='form-control' for='portion-size' id='portion-size'>" +
+        "<div class='form-note'>(i.e. Gallon, Quart, Pint, Cup etc.)</div>" +
+        "<input class='form-control' name = 'portion-size' for='portion-size' id='portion-size' required>" +
         "<label for='quantity'>Quantity: </label> " +
-        "<input class='form-control' for='quantity' id='quantity' type='number'>" +
-      "<button class='btn btn-default'>Submit</button>";
-  $('.form').html(formHtml);
+        "<input class='form-control' for='quantity' id='quantity' type='number' name='quantity' required>" +
+        "<label for='ingredients'>Ingredients: </label>" +
+        "<div class='form-note'>(please separate with a comma)</div>" +
+        "<input class='form-control' for='ingredients' id='ingredients' name='ingredients'>" +
+      "</fieldset>" +
+      "<button class='submit btn btn-default'>Submit</button>" +
+    "</form>";
+  $('.formHolder').html(formHtml);
 };
 
 $(document).ready(function () {
   addItemForm();
+
+  $('.formHolder').on('click', '.submit', function () {
+    event.preventDefault();
+    console.log('form is done.');
+    console.log($('form.addForm').serialize());
+  });
+
 });
