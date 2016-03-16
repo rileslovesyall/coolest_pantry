3bd0fc01ac8ecc7c7f53529feecec0038c155355
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-16 10:58:29 -0700

    updated and reformatted about page

--- public/app.js
+++ public/app.js
@@ -72,15 +72,21 @@ var displaySplash = function () {
 
 var displayAbout = function () {
   var aboutHTML =
-  "<p>Do you preserve food at home? Are you comforted by rows of jars lining your pantry shelves, or filling your freezer? Do you ever lose track of what all you have in the midst of such plenty?</p>" +
+  "<p class='about-small'>Do you preserve food at home? Are you comforted by rows of jars lining your pantry shelves, or filling your freezer? Do you ever lose track of what all you have in the midst of such plenty?</p>" +
 
-"<p>Welcome to Pocket Pantry. You can easily track all of your preserved goods, with their ingredients and expiration dates, get weekly reminders of what needs to be eaten soon, and see at a glance what you have available.</p>" +
+"<h2 class='about-header-section'>Welcome to Pocket Pantry. Easily track your preserved goods with their ingredients and expiration dates, get weekly reminders of what needs to be eaten soon, & see at a glance what's available.</h2>" +
 
-"<p> To get started, create a new item, including it's ingredients, and how long it takes (from the date it was made) to expire. You can include an initial quantity when you log your first item, if you have any in stock. You can set the portion sizes your item is preserved in, like 'cups,' 'pints,' 'quarts,' or whatever size you'd like. Each time you use that amount, you can log it using the 'consume' button on the item page. In the future, you'll be able to see a visualization of when you've consumed your items, giving you more data to plan your future food needs.</p>" +
+"<p>To get started, click on 'Add Item' under the 'My Pantry' tab. Each time you pull from your pantry, log it using the 'consume' button on the item page. In the future, you'll be able to see a visualization of when you've consumed your items, giving you more data to plan your future food needs.</p>" +
 
-"<p>Whenever you make more of the same item, just use the 'add' button on the item page, and the new quantity will immediately be added to your pantry with the proper expiration date.</p>" +
+"<p>Whenever you make more of something, use the 'add' button on the item page. Use a few different ingredients? 'Copy' the item, give it a slightly different name, and log your latest creation.</p>" +
 
-"<p>On the 'My Account' tab, you can edit your notification settings. By default, you will be sent a weekly e-mail listing any items expiring in the next two weeks, as well as a list of all the items in your current pantry. You're welcome to turn off these e-mails, as well as change what you deem as 'expiring soon.' (Want to see everything that expires in the next month? Just change that setting to '4 weeks' instead of the default 2!)</p>";
+"<p class='about-small'>On the 'My Account' tab, you can edit your notification settings. By default, you will be sent a weekly e-mail listing any items expiring in the next two weeks. You're welcome to turn off these e-mails, as well as change what you see as 'expiring soon.' (Want to see everything that expires in the next month? Just change that setting to '4 weeks' instead of the default 2!)</p>";
+
+// aboutHTML +=
+//  "<div class='divider'></div>" +
+//  "<h1 class='header'>About the Creator</h1>" +
+
+//  "<h2>Pocket Pantry was created by <a href='http://rileyspicer.com'>Riley Spicer</a> as a capstone project for <a href='http://adadevelopersacademy.org'>Ada Developer's Academy</a>.</h2>";
 
   $('#header').text("About Pocket Pantry");
   $('.about').show();
